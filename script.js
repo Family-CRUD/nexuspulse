45de224333d6a21a2aa243ac4ee0911437c2a6ee
mustbeneru <neilcarlo.zapanta.d@gmail.com>
2024-03-04 18:05:26 +0800

    fix script file

--- script.js
+++ script.js
@@ -21,6 +21,7 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     }
 
+    
     const loginForm = document.querySelector('.login-form');
     if (loginForm) {
         loginForm.addEventListener('click', () => {
