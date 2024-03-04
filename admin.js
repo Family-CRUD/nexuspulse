dafe0e87574722e28dd3bcd0c07a77bac662869a
mustbeneru <neilcarlo.zapanta.d@gmail.com>
2024-03-04 13:29:48 +0800

    fix some errors

--- admin.js
+++ admin.js
@@ -1,12 +1,25 @@
-const dashboard = document.querySelector('.dashboard')
-const patientData = document.querySelector('.patient-records')
+// const dashboard = document.querySelector('.dashboard')
+// const patientData = document.querySelector('.patient-records')
 
-dashboard.addEventListener('click', () => {
-    dashboard.classList.add('stay')
-    patientData.classList.remove('stay')
+// dashboard.addEventListener('click', () => {
+//     dashboard.classList.add('stay')
+//     patientData.classList.remove('stay')
+// })
+
+// patientData.addEventListener('click', () => {
+//     patientData.classList.add('stay')
+//     dashboard.classList.remove('stay')
+// })
+
+const viewButton = document.querySelectorAll('.view')
+const dashboard = document.querySelector('.dashboard') 
+
+viewButton.forEach((button) => {
+    button.addEventListener('click', () => {
+        window.location.href = '../html/view-patient.html'
+    })
 })
 
-patientData.addEventListener('click', () => {
-    patientData.classList.add('stay')
-    dashboard.classList.remove('stay')
+dashboard.addEventListener('click', () => {
+    window.location.href = '../html/dashboard.html'
 })
---
+++ dashboard.js
@@ -0,0 +1,5 @@
+const records = document.querySelector('.records')
+
+records.addEventListener('click', () => {
+    window.location.href = '../html/admin.html'
+})
